9de9ba3d8a56a47767b33dc0a30820459973e4c0
TLtlbo <js1436kt@gmail.com>
2024-04-15 15:27:47 +0900

    Initial Commit

---
+++ PinforYou/PinforYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinforYouApp.swift
+//  PinforYou
+//
+//  Created by 박진성 on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct PinforYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
