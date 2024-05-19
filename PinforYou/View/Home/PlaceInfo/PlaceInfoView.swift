4790162f3a1eb702e5469a1c6376680f546469a6
TLtlbo <js1436kt@gmail.com>
2024-05-20 00:22:48 +0900

    해당 위치에서 결제를 선택할 카드를 보여주는 뷰 구현

--- PinforYou/View/Home/PlaceInfo/PlaceInfoView.swift
+++ PinforYou/View/Home/PlaceInfo/PlaceInfoView.swift
@@ -10,12 +10,39 @@ import SwiftUI
 struct PlaceInfoView: View {
     
     @Binding var Place : PlaceModel.Place
+    
+    //TEST
+    var testCardList : [Card] = [Card.cardStub1, Card.cardStub2, Card.cardStub3, Card.cardStub4]
+    
+    
     var body: some View {
         
         VStack {
             HStack {
                 PlaceCell(Place: Place)
                 Spacer()
+            }
+            
+            Rectangle()
+                .fill(Color.blue)
+                .frame(height: 2)
+                .padding(.bottom, 0)
+            
+            ScrollView(.vertical) {
+                VStack(spacing: 0) {
+                    Rectangle()
+                        .fill(Color.gray)
+                        .frame(height: 1)
+                    
+                    ForEach(testCardList, id: \.self) { card in
+                        CardCell()
+                        
+                        Rectangle()
+                            .fill(Color.white)
+                            .frame(height:1)
+                    }
+                    
+                }
             }
             
             
