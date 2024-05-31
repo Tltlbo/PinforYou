c5fa411b73af0a67577660323973c1629e9c5157
TLtlbo <js1436kt@gmail.com>
2024-05-31 11:52:18 +0900

    테스트를 위한 임시 이벤트 뷰 구현

---
+++ PinforYou/View/Event/EventView.swift
@@ -0,0 +1,18 @@
+//
+//  EventView.swift
+//  PinforYou
+//
+//  Created by 박진성 on 5/31/24.
+//
+
+import SwiftUI
+
+struct EventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventView()
+}
