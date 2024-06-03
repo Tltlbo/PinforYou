8a5eb72e7024e7db4b778bfc050d2e84b82fe9a9
TLtlbo <js1436kt@gmail.com>
2024-06-03 10:25:03 +0900

    전체 메뉴 뷰 임시 구현

--- PinforYou/View/MainTabView/MainTabView.swift
+++ PinforYou/View/MainTabView/MainTabView.swift
@@ -29,7 +29,7 @@ struct MainTabView: View {
                     case .Event:
                         EventView()
                     case .AllMenu:
-                        Color.cyan
+                        AllMenuView()
                     }
                 }
                 .tabItem {
