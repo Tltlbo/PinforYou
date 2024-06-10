fc9dc111d850763e7884aae06c8fff78781717b2
TLtlbo <js1436kt@gmail.com>
2024-06-10 23:20:50 +0900

    내 카드 탭 화면 분리 작업 및 각 리스트 스크롤 뷰가 탭을 간섭하는 현상 제거

---
+++ PinforYou/View/CardList/MyCardView.swift
@@ -0,0 +1,18 @@
+//
+//  MyCardView.swift
+//  PinforYou
+//
+//  Created by 박진성 on 6/10/24.
+//
+
+import SwiftUI
+
+struct MyCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyCardView()
+}
