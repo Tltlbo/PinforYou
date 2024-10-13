c5d46f1ce9098f0061fafe81152a9e97e1716211
TLtlbo <js1436kt@gmail.com>
2024-10-13 16:46:31 +0900

    [FEAT] 회원가입 2 화면 구성

--- PinforYou/View/Login/SignUp/SignUpUserInfoView.swift
+++ PinforYou/View/Login/SignUp/SignUpUserInfoView.swift
@@ -7,54 +7,98 @@
 
 import SwiftUI
 
+enum Gender: String, CaseIterable {
+    case male = "남자"
+    case female = "여자"
+}
+
 struct SignUpUserInfoView: View {
     
     @EnvironmentObject var authViewModel : AuthenticationViewModel
+    @State var name: String = ""
+    @State var phoneNumber: String = ""
+    @State var gender: Gender = .male
+    @State var age: String = ""
     
     var body: some View {
         NavigationStack {
-            VStack(alignment: .leading) {
-                
-                HStack {
-                    Text("""
-                        사용자 정보를
-                        입력해주세요!
-                        """)
-                    .font(.system(size: 26, weight: .bold))
+            ScrollView {
+                VStack(alignment: .leading) {
+                    
+                    HStack {
+                        Text("""
+                            사용자 정보를
+                            입력해주세요!
+                            """)
+                        .font(.system(size: 26, weight: .bold))
+                        
+                        Spacer()
+                    }
+                    .padding(.top, 50)
+                    
+                    VStack(alignment: .leading) {
+                        Text("이름")
+                        TextField("이름을 입력해주세요", text: $name)
+                            .autocorrectionDisabled(true)
+                            .keyboardType(.namePhonePad)
+                        Divider()
+                    }
+                    .padding(.top, 48)
+                    
+                    
+                    VStack(alignment: .leading) {
+                        Text("휴대폰 번호(-를 제외한)")
+                        TextField("휴대폰 번호를 입력해주세요", text: $name)
+                            .autocorrectionDisabled(true)
+                            .keyboardType(.numberPad)
+                        Divider()
+                    }
+                    .padding(.top, 52)
+                    
+                    VStack(alignment: .leading) {
+                        Text("성별")
+                        Picker("성별을 선택해주세요", selection: $gender) {
+                            ForEach(Gender.allCases, id: \.self) { gender in
+                                Text(gender.rawValue).tag(gender)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                    }
+                    .padding(.top, 52)
+                    
+                    VStack(alignment: .leading) {
+                        Text("나이")
+                        TextField("나이를 입력해주세요", text: $name)
+                            .autocorrectionDisabled(true)
+                            .keyboardType(.numberPad)
+                        Divider()
+                    }
+                    .padding(.top, 52)
                     
                     Spacer()
-                }
-                .padding(.top, 80)
-                
-                
-                Text("이름")
-                    .padding(.top, 48)
-                
-                Text("휴대폰 번호")
-                    .padding(.top, 82)
-                
-                Text("성별")
-                    .padding(.top, 82)
-                
-                Text("나이")
-                    .padding(.top, 82)
-                
-                Spacer()
-                
-                NavigationLink {
-                    //
-                } label: {
-                    HStack(alignment: .center) {
-                        Text("완료")
+                    
+                    Button {
+                        //
+                    } label: {
+                        HStack(alignment: .center) {
+                            Text("완료")
+                        }
+                        .frame(width: UIScreen.main.bounds.size.width - 20, height: 72)
+                        .background(Color("CellBackgroundColor"))
+                        .clipShape(.rect(cornerRadius: 20))
                     }
-                    .frame(width: UIScreen.main.bounds.size.width - 20, height: 72)
-                    .background(Color("CellBackgroundColor"))
-                    .clipShape(.rect(cornerRadius: 20))
                 }
-
-                
+                .padding(.leading, 10)
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button {
+                            authViewModel.authenticationState = .unauthenticated
+                        } label: {
+                            Image(systemName: "chevron.left")
+                        }
+                    }
+                }
             }
-            .padding(.leading, 10)
         }
     }
 }
